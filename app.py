19fca292611e6fe75b24b5071194eafdd3e762fe
Ryan <ryanang333@gmail.com>
2026-08-11 14:54:16 +0800

    showdown - courtesy of chat

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask import Flask
 from routes.health import health
 from routes.hello import hello
 from routes.solve import solve
+from routes.move import move
 
 
 app = Flask(__name__)
@@ -14,6 +15,7 @@ def register_routes(flask_app: Flask) -> None:
     flask_app.add_url_rule("/", "hello", hello, methods=["GET"])
     flask_app.add_url_rule("/solve", "solve", solve, methods=["POST"])
     flask_app.add_url_rule("/health", "health", health, methods=["GET"])
+    flask_app.add_url_rule("/move", "move", move, method=["POST"])
 
 
 register_routes(app)
